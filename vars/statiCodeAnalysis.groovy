a875d4c8fc7ecaf739e77c1e28face81c54fdeed
Tenjomo <38832741+tenjomo@users.noreply.github.com>
2023-07-16 23:09:39 +0200

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(credentialsId){
 
     withSonarQubeEnv(credentialsId: credentialsId) {
-         bat 'mvn clean package sonar:sonar'
+         'cmd /c mvn clean package sonar:sonar'.execute()
     }
 }
